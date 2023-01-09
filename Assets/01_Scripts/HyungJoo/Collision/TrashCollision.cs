c02a677e5c1d5a4917de3b019fbdc8c27da4e32c
nacfson <nacfson@gmail.com>
2023-01-09 20:05:38 +0900

    dd

--- Assets/01_Scripts/HyungJoo/Collision/TrashCollision.cs
+++ Assets/01_Scripts/HyungJoo/Collision/TrashCollision.cs
@@ -7,8 +7,12 @@ public class TrashCollision : MonoBehaviour
     public float speed = 0.2f;
 
     private void FixedUpdate() {
-        transform.position -= new Vector3(0,speed * 0.05f);
-        transform.Rotate(new Vector3(0,0,speed * 10f));
+        if(GameManager.Instance.canMove)
+        {
+            transform.position -= new Vector3(0,speed * 0.05f);
+            transform.Rotate(new Vector3(0,0,speed * 10f));
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.collider.CompareTag("Player"))
